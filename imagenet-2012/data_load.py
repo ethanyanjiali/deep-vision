7856b2d90f142b224f021a561aeca4d4b613fc5f
Ethan Yanjia Li <liyanjia92@gmail.com>
2018-12-29 23:05:35 -0800

    .float()

--- imagenet-2012/data_load.py
+++ imagenet-2012/data_load.py
@@ -187,7 +187,7 @@ class ToTensor(object):
         image = image.transpose((2, 0, 1))
 
         return {
-            'image': torch.from_numpy(image),
+            'image': torch.from_numpy(image).float(),
             'annotation': annotation,
         }
 
