ef1e40f04693109f49ec7c66004082d1e4667fb3
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-05-22 14:32:44 -0700

    remove imshow

--- GANs/DCGAN/main.py
+++ GANs/DCGAN/main.py
@@ -78,11 +78,9 @@ def main():
 
         for i in range(predictions.shape[0]):
             plt.subplot(4, 4, i + 1)
-            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
             plt.axis('off')
 
         plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-        plt.show()
 
     def train(dataset, epochs):
         for epoch in range(1, epochs+1):
