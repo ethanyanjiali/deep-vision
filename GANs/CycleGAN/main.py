0ef584b9afe23bfde90ee4252ca0ed0fe2f0c7ea
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-05-26 14:24:15 -0700

    fix losses

--- GANs/CycleGAN/main.py
+++ GANs/CycleGAN/main.py
@@ -14,8 +14,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 LEARNING_RATE = 0.0002
 BETA_1 = 0.5
-LAMBDA_A = 10.0
-LAMBDA_B = 10.0
+LAMBDA = 10.0
 LAMBDA_ID = 0.5
 POOL_SIZE = 50
 EPOCHS = 100
@@ -30,32 +29,14 @@ def main():
         '--batch_size', help='The batch size of input data', default='2')
     args = parser.parse_args()
 
-    loss_G_A_metrics = tf.keras.metrics.Mean('loss_G_A_metrics', dtype=tf.float32)
-    loss_G_B_metrics = tf.keras.metrics.Mean('loss_G_B_metrics', dtype=tf.float32)
-    loss_D_A_metrics = tf.keras.metrics.Mean('loss_D_A_metrics', dtype=tf.float32)
-    loss_D_B_metrics = tf.keras.metrics.Mean('loss_D_B_metrics', dtype=tf.float32)
+    loss_gen_a2b_metrics = tf.keras.metrics.Mean('loss_gen_a2b_metrics', dtype=tf.float32)
+    loss_gen_b2a_metrics = tf.keras.metrics.Mean('loss_gen_b2a_metrics', dtype=tf.float32)
+    loss_dis_b_metrics = tf.keras.metrics.Mean('loss_dis_b_metrics', dtype=tf.float32)
+    loss_dis_a_metrics = tf.keras.metrics.Mean('loss_dis_a_metrics', dtype=tf.float32)
     mse_loss = tf.keras.losses.MeanSquaredError()
     mae_loss = tf.keras.losses.MeanAbsoluteError()
-    fake_pool_A = ImagePool(POOL_SIZE)
-    fake_pool_B = ImagePool(POOL_SIZE)
-
-    def query_image_pool(fake_images, fake_pool):
-        if len(fake_pool) < POOL_SIZE:
-            fake_pool.append(fake_images)
-            tf.print('here 1', len(fake_pool))
-            return fake_images
-        else:
-            tf.print('here 2', len(fake_pool))
-            p = random.random()
-            if p > 0.5:
-                tf.print('here 3')
-                random_id = random.randint(0, POOL_SIZE-1)
-                temp = fake_pool[random_id]
-                fake_pool[random_id] = fake_images
-                return temp
-            else:
-                tf.print('here 4')
-                return fake_images
+    fake_pool_b2a = ImagePool(POOL_SIZE)
+    fake_pool_a2b = ImagePool(POOL_SIZE)
 
     def calc_gan_loss(prediction, is_real):
         # Typical GAN loss to set objectives for generator and discriminator
@@ -73,94 +54,96 @@ def main():
         # Ideally, feeding a real image to generator should generate itself
         return mae_loss(identity_images, real_images)
 
-    netG_A = make_generator_model(n_blocks=9)
-    netG_B = make_generator_model(n_blocks=9)
-    netD_A = make_discriminator_model()
-    netD_B = make_discriminator_model()
-    optimizer_G_A = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
-    optimizer_D_A = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
-    optimizer_G_B = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
-    optimizer_D_B = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+    generator_a2b = make_generator_model(n_blocks=9)
+    generator_b2a = make_generator_model(n_blocks=9)
+    discriminator_b = make_discriminator_model()
+    discriminator_a = make_discriminator_model()
+    optimizer_gen_a2b = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+    optimizer_dis_b = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+    optimizer_gen_b2a = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
+    optimizer_dis_a = tf.keras.optimizers.Adam(LEARNING_RATE, BETA_1)
 
     checkpoint_dir = './checkpoints-{}'.format(args.dataset)
-    checkpoint = tf.train.Checkpoint(netG_A=netG_A,
-                                     netG_B=netG_B,
-                                     netD_A=netD_A,
-                                     netD_B=netD_B,
-                                     optimizer_G_A=optimizer_G_A,
-                                     optimizer_D_A=optimizer_D_A,
-                                     optimizer_G_B=optimizer_G_B,
-                                     optimizer_D_B=optimizer_D_B,
+    checkpoint = tf.train.Checkpoint(generator_a2b=generator_a2b,
+                                     generator_b2a=generator_b2a,
+                                     discriminator_b=discriminator_b,
+                                     discriminator_a=discriminator_a,
+                                     optimizer_gen_a2b=optimizer_gen_a2b,
+                                     optimizer_dis_b=optimizer_dis_b,
+                                     optimizer_gen_b2a=optimizer_gen_b2a,
+                                     optimizer_dis_a=optimizer_dis_a,
                                      step=tf.Variable(0))
     checkpoint_manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=3)
 
-    def train_step(images_A, images_B):
-        real_A = images_A
-        real_B = images_B
+    def train_step(images_a, images_b):
+        real_a = images_a
+        real_b = images_b
 
         # By default, the resources held by a GradientTape are released as soon as GradientTape.gradient()
         # method is called. To compute multiple gradients over the same computation, create a persistent gradient tape.
         # This allows multiple calls to the gradient() method as resources are released
         # when the tape object is garbage collected.
-        with tf.GradientTape() as tapeG_A, tf.GradientTape() as tapeG_B, tf.GradientTape() as tapeD_A, tf.GradientTape() as tapeD_B:
+        with tf.GradientTape() as tape_gen_a2b, tf.GradientTape() as tape_gen_b2a, tf.GradientTape() as tape_dis_b, tf.GradientTape() as tape_dis_a:
             # Cycle A -> B -> A
-            fake_B = netG_A(real_A, training=True)
-            recon_A = netG_B(fake_B, training=True)
+            fake_a2b = generator_a2b(real_a, training=True)
+            recon_b2a = generator_b2a(fake_a2b, training=True)
             # Cycle B -> A -> B
-            fake_A = netG_B(real_B, training=True)
-            recon_B = netG_A(fake_A, training=True)
+            fake_b2a = generator_b2a(real_b, training=True)
+            recon_a2b = generator_a2b(fake_b2a, training=True)
 
             # Use real B to generate B should be identical
-            identity_A = netG_A(real_B, training=True)
-            identity_B = netG_B(real_A, training=True)
-            loss_identity_A = calc_identity_loss(identity_A, real_B)
-            loss_identity_B = calc_identity_loss(identity_B, real_A)
+            identity_a2b = generator_a2b(real_b, training=True)
+            identity_b2a = generator_b2a(real_a, training=True)
+            loss_identity_a2b = calc_identity_loss(identity_a2b, real_b)
+            loss_identity_b2a = calc_identity_loss(identity_b2a, real_a)
 
-            # Generator tries to trick Discriminator
-            loss_gan_G_A = calc_gan_loss(netD_A(fake_B), True)
-            loss_gan_G_B = calc_gan_loss(netD_B(fake_A), True)
-            loss_cycle_A = calc_cycle_loss(recon_A, real_A)
-            loss_cycle_B = calc_cycle_loss(recon_B, real_B)
+            # Generator A2B tries to trick Discriminator B that the generated image is B
+            loss_gan_gen_a2b = calc_gan_loss(discriminator_b(fake_a2b), True)
+            # Generator B2A tries to trick Discriminator A that the generated image is A
+            loss_gan_gen_b2a = calc_gan_loss(discriminator_a(fake_b2a), True)
+            loss_cycle_a2b2a = calc_cycle_loss(recon_b2a, real_a)
+            loss_cycle_b2a2b = calc_cycle_loss(recon_a2b, real_b)
 
-            loss_G_A = loss_gan_G_A + loss_cycle_A * LAMBDA_A + loss_identity_A * LAMBDA_A * LAMBDA_ID
-            loss_G_B = loss_gan_G_B + loss_cycle_B * LAMBDA_B + loss_identity_B * LAMBDA_B * LAMBDA_ID
+            # Total generator loss
+            loss_total_gen_a2b = loss_gan_gen_a2b + (loss_cycle_a2b2a + loss_cycle_b2a2b) * LAMBDA + loss_identity_a2b * LAMBDA * LAMBDA_ID
+            loss_total_gen_b2a = loss_gan_gen_b2a + (loss_cycle_a2b2a + loss_cycle_b2a2b) * LAMBDA + loss_identity_b2a * LAMBDA * LAMBDA_ID
 
-            fake_A_to_inspect = fake_pool_A.query(fake_A)
-            decision_B_real = netD_B(real_A, training=True)
-            decision_B_fake = netD_B(fake_A_to_inspect, training=True)
-            # For discriminator, true is true, false is false
-            loss_gan_D_B_real = calc_gan_loss(decision_B_real, True)
-            loss_gan_D_B_fake = calc_gan_loss(decision_B_fake, False)
-            loss_D_B = (loss_gan_D_B_real + loss_gan_D_B_fake) * 0.5
+            fake_b2a_from_pool = fake_pool_b2a.query(fake_b2a)
+            # Discriminator A should classify real_a as A
+            loss_gan_dis_a_real = calc_gan_loss(discriminator_a(real_a, training=True), True)
+            # Discriminator A should classify generated fake_b2a as not A
+            loss_gan_dis_a_fake = calc_gan_loss(discriminator_a(fake_b2a_from_pool, training=True), False)
 
-            fake_B_to_inspect = fake_pool_B.query(fake_B)
-            decision_A_real = netD_A(real_B, training=True)
-            decision_A_fake = netD_A(fake_B_to_inspect, training=True)
-            # For discriminator, true is true, false is false
-            loss_gan_D_A_real = calc_gan_loss(decision_A_real, True)
-            loss_gan_D_A_fake = calc_gan_loss(decision_A_fake, False)
-            loss_D_A = (loss_gan_D_A_real + loss_gan_D_A_fake) * 0.5
+            fake_a2b_from_pool = fake_pool_a2b.query(fake_a2b)
+            # Discriminator B should classify real_b as B
+            loss_gan_dis_b_real = calc_gan_loss(discriminator_b(real_b, training=True), True)
+            loss_gan_dis_b_fake = calc_gan_loss(discriminator_b(fake_a2b_from_pool, training=True), False)
 
-        gradientG_A = tapeG_A.gradient(loss_G_A, netG_A.trainable_variables)
-        gradientG_B = tapeG_B.gradient(loss_G_B, netG_B.trainable_variables)
-        gradientD_A = tapeD_A.gradient(loss_D_A, netD_A.trainable_variables)
-        gradientD_B = tapeD_B.gradient(loss_D_B, netD_B.trainable_variables)
+            # Total discriminator loss
+            loss_dis_a = (loss_gan_dis_a_real + loss_gan_dis_a_fake) * 0.5
+            loss_dis_b = (loss_gan_dis_b_real + loss_gan_dis_b_fake) * 0.5
 
-        optimizer_G_A.apply_gradients(zip(gradientG_A, netG_A.trainable_variables))
-        optimizer_D_A.apply_gradients(zip(gradientD_A, netD_A.trainable_variables))
-        optimizer_G_B.apply_gradients(zip(gradientG_B, netG_B.trainable_variables))
-        optimizer_D_B.apply_gradients(zip(gradientD_B, netD_B.trainable_variables))
+        gradient_gen_a2b = tape_gen_a2b.gradient(loss_total_gen_a2b, generator_a2b.trainable_variables)
+        gradient_gen_b2a = tape_gen_b2a.gradient(loss_total_gen_b2a, generator_b2a.trainable_variables)
+        gradient_dis_a = tape_dis_a.gradient(loss_dis_a, discriminator_a.trainable_variables)
+        gradient_dis_b = tape_dis_b.gradient(loss_dis_b, discriminator_b.trainable_variables)
 
-        loss_G_A_metrics(loss_G_A)
-        loss_G_B_metrics(loss_G_B)
-        loss_D_A_metrics(loss_D_A)
-        loss_D_B_metrics(loss_D_B)
-        tf.print('loss_G_A: ', loss_G_A, ' loss_G_B: ', loss_G_B, ' loss_D_A: ', loss_D_A, ' loss_D_B: ', loss_D_B)
+        optimizer_gen_a2b.apply_gradients(zip(gradient_gen_a2b, generator_a2b.trainable_variables))
+        optimizer_gen_b2a.apply_gradients(zip(gradient_gen_b2a, generator_b2a.trainable_variables))
+        optimizer_dis_a.apply_gradients(zip(gradient_dis_a, discriminator_a.trainable_variables))
+        optimizer_dis_b.apply_gradients(zip(gradient_dis_b, discriminator_b.trainable_variables))
+
+        loss_gen_a2b_metrics(loss_total_gen_a2b)
+        loss_gen_b2a_metrics(loss_total_gen_b2a)
+        loss_dis_a_metrics(loss_dis_a)
+        loss_dis_b_metrics(loss_dis_b)
+        tf.print('loss_total_gen_a2b: ', loss_total_gen_a2b, ' loss_total_gen_b2a: ', loss_total_gen_b2a, ' loss_dis_b: ', loss_dis_b, ' loss_dis_a: ', loss_dis_a)
 
     current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     train_log_dir = 'logs/horse2zebra/' + current_time + '/train'
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 
+    # Eager mode
     def train(dataset, epochs):
         for epoch in range(1, epochs+1):
             start = time.time()
@@ -169,20 +152,20 @@ def main():
                 train_step(batch[0], batch[1])
 
             with train_summary_writer.as_default():
-                tf.summary.scalar('loss_G_A', loss_G_A_metrics.result(), step=epoch)
-                tf.summary.scalar('loss_G_B', loss_G_B_metrics.result(), step=epoch)
-                tf.summary.scalar('loss_D_A', loss_D_A_metrics.result(), step=epoch)
-                tf.summary.scalar('loss_D_B', loss_D_B_metrics.result(), step=epoch)
+                tf.summary.scalar('loss_total_gen_a2b', loss_gen_a2b_metrics.result(), step=epoch)
+                tf.summary.scalar('loss_total_gen_b2a', loss_gen_b2a_metrics.result(), step=epoch)
+                tf.summary.scalar('loss_dis_b', loss_dis_b_metrics.result(), step=epoch)
+                tf.summary.scalar('loss_dis_a', loss_dis_a_metrics.result(), step=epoch)
 
             tf.print('Epoch ', epoch,
-                     ' avg loss_G_A: ', loss_G_A_metrics.result(),
-                     ' avg loss_G_B: ', loss_G_B_metrics.result(),
-                     ' avg loss_D_A: ', loss_D_A_metrics.result(),
-                     ' avg loss_D_B: ', loss_D_B_metrics.result())
-            loss_G_A_metrics.reset_states()
-            loss_G_B_metrics.reset_states()
-            loss_D_A_metrics.reset_states()
-            loss_D_B_metrics.reset_states()
+                     ' avg loss_total_gen_a2b: ', loss_gen_a2b_metrics.result(),
+                     ' avg loss_total_gen_b2a: ', loss_gen_b2a_metrics.result(),
+                     ' avg loss_dis_b: ', loss_dis_b_metrics.result(),
+                     ' avg loss_dis_a: ', loss_dis_a_metrics.result())
+            loss_gen_a2b_metrics.reset_states()
+            loss_gen_b2a_metrics.reset_states()
+            loss_dis_b_metrics.reset_states()
+            loss_dis_a_metrics.reset_states()
 
             checkpoint.step.assign_add(1)
             if epoch % 5 == 0:
@@ -219,15 +202,15 @@ def main():
         parsed_image_dataset = raw_dataset.map(parse_image_function)
         return parsed_image_dataset
 
-    train_A = make_dataset('tfrecords/{}/trainA.tfrecord'.format(args.dataset))
-    train_B = make_dataset('tfrecords/{}/trainB.tfrecord'.format(args.dataset))
-    combined_dataset = tf.data.Dataset.zip((train_A, train_B)).shuffle(SHUFFLE_SIZE).batch(int(args.batch_size))
+    train_a = make_dataset('tfrecords/{}/trainA.tfrecord'.format(args.dataset))
+    train_b = make_dataset('tfrecords/{}/trainB.tfrecord'.format(args.dataset))
+    combined_dataset = tf.data.Dataset.zip((train_a, train_b)).shuffle(SHUFFLE_SIZE).batch(int(args.batch_size))
 
     # for local testing
-    seed1 = tf.random.normal([2, 256, 256, 3])
-    seed2 = tf.random.normal([2, 256, 256, 3])
-    combined_dataset = [(seed1, seed2)]
-    EPOCHS = 2
+    # seed1 = tf.random.normal([2, 256, 256, 3])
+    # seed2 = tf.random.normal([2, 256, 256, 3])
+    # combined_dataset = [(seed1, seed2)]
+    # EPOCHS = 2
 
     train(combined_dataset, EPOCHS)
     print('Finished training.')
