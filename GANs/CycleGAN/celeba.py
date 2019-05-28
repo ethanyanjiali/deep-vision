6c7ed9d3dc421432f4723f306a30bfcf1156308d
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-05-27 18:33:07 -0700

    update

--- GANs/CycleGAN/celeba.py
+++ GANs/CycleGAN/celeba.py
@@ -10,10 +10,10 @@ with open('./list_attr_celeba.txt') as fp:
         print(gender)
         if int(gender) == -1:
             print(filename, 'female')
-            copyfile('./img_align_celeba/{}'.format(filename), './celeba/trainB/{}'.format(filename))
+            copyfile('./img_align_celeba/{}'.format(filename), './datasets/celeba/trainB/{}'.format(filename))
         elif int(gender) == 1:
             print(filename, 'male')
-            copyfile('./img_align_celeba/{}'.format(filename), './celeba/trainA/{}'.format(filename))
+            copyfile('./img_align_celeba/{}'.format(filename), './datasets/celeba/trainA/{}'.format(filename))
         else:
             raise Exception('Unknown gender')
         line = fp.readline()
