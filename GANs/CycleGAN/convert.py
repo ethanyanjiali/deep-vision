38a63d7741a72f7289e875327a2e346b4da708b2
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-05-29 12:14:15 -0700

    monet2photo

--- GANs/CycleGAN/convert.py
+++ GANs/CycleGAN/convert.py
@@ -5,11 +5,11 @@ import tensorflow as tf
 
 
 def convert():
-    saved_mode_dir = './saved_models/celeba/male2female/201905282111/'
+    saved_mode_dir = './saved_models/monet2photo/photo2monet/201905291209/'
     converter = tf.lite.TFLiteConverter.from_saved_model(saved_mode_dir)
     converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
     tflite_model = converter.convert()
-    open("./converted_model.tflite", "wb").write(tflite_model)
+    open("./photo2monet_converted_model.tflite", "wb").write(tflite_model)
     return
 
 
--- GANs/CycleGAN/inference.py
+++ GANs/CycleGAN/inference.py
@@ -12,7 +12,8 @@ def main():
     generator_a2b = make_generator_model(n_blocks=9)
     generator_b2a = make_generator_model(n_blocks=9)
 
-    checkpoint_dir = './checkpoints-celeba'
+    ds = 'monet2photo'
+    checkpoint_dir = './checkpoints-{}'.format(ds)
     checkpoint = tf.train.Checkpoint(generator_a2b=generator_a2b,
                                      generator_b2a=generator_b2a)
     manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=3)
@@ -23,12 +24,12 @@ def main():
         print("Initializing from scratch.")
 
     # Load TFLite model and allocate tensors.
-    # interpreter = tf.lite.Interpreter(model_path="converted_model.tflite")
-    # interpreter.allocate_tensors()
+    interpreter = tf.lite.Interpreter(model_path="photo2monet_converted_model.tflite")
+    interpreter.allocate_tensors()
 
     # Get input and output tensors.
-    # input_details = interpreter.get_input_details()
-    # output_details = interpreter.get_output_details()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
     def generate(filename, idx, kind):
         with open(filename, 'rb') as f:
@@ -40,10 +41,10 @@ def main():
         inputs = tf.expand_dims(inputs, 0)
 
         # outputs = generator_b2a(inputs)
-        outputs = generator_a2b(inputs)
-        # interpreter.set_tensor(input_details[0]['index'], inputs)
-        # interpreter.invoke()
-        # outputs = interpreter.get_tensor(output_details[0]['index'])
+        # outputs = generator_a2b(inputs)
+        interpreter.set_tensor(input_details[0]['index'], inputs)
+        interpreter.invoke()
+        outputs = interpreter.get_tensor(output_details[0]['index'])
 
         generated = outputs[0]
         generated = (generated + 1) * 127.5
@@ -54,20 +55,21 @@ def main():
         # plt.show()
         # print(original)
         im1 = Image.fromarray(original.numpy())
-        im1.save('samples_celeba/{}_{}_original.JPEG'.format(kind, idx))
-        generated = tf.image.resize(generated, (218, 178))
+        im1.save('samples_{}/custom_{}_{}_original.JPEG'.format(ds, kind, idx))
+        # generated = tf.image.resize(generated, (218, 178))
         generated = tf.cast(generated, tf.uint8)
         im2 = Image.fromarray(generated.numpy())
-        im2.save('samples_celeba/{}_{}_generated.JPEG'.format(kind, idx))
+        im2.save('samples_{}/custom_{}_{}_generated.JPEG'.format(ds, kind, idx))
 
-    # files = glob.glob(os.path.join('datasets', 'horse2zebra', 'testB', '*'))
-    generate('/Users/yanjia.li/Downloads/img_align_celeba/000030.jpg', 30, 'male2female')
-    # for idx in range(120):
-    #    generate(files[idx], idx, 'male2female')
+    files = glob.glob(os.path.join('datasets', ds, 'customB', '*'))
+    # generate('/Users/yanjia.li/Downloads/img_align_celeba/000030.jpg', 30, 'male2female')
+    for idx in range(2):
+        generate(files[idx], idx, 'photo2monet')
+        # generate(files[idx], idx, 'monet2photo')
 
     # tf.saved_model.save(generator_a2b, "./saved_models/celeba/male2female/201905282111/")
 
-    # tf.saved_model.save(generator_b2a, "./saved_models/celeba/female2male/201905282111/")
+    tf.saved_model.save(generator_b2a, "./saved_models/monet2photo/photo2monet/201905291209/")
 
 
 if __name__ == '__main__':
