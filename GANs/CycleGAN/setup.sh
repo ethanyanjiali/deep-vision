d623d170d94a465a883b1f66bf94c3794da9069c
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-05-29 01:30:52 -0700

    setup

--- GANs/CycleGAN/setup.sh
+++ GANs/CycleGAN/setup.sh
@@ -3,9 +3,9 @@ python -m venv env
 source env/bin/activate
 pip install -r requirements_gpu.in
 mkdir datasets
-wget https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip
-unzip horse2zebra.zip
-rm horse2zebra.zip
-mv horse2zebra ./datasets/
-mkdir -p tfrecords/horse2zebra
-python tfrecords.py --dataset=horse2zebra
+wget https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/monet2photo.zip
+unzip monet2photo.zip
+rm monet2photo.zip
+mv monet2photo ./datasets/
+mkdir -p tfrecords/monet2photo
+python tfrecords.py --dataset=monet2photo
