9c2affc9b818e1b00671cd6d4c6526c11d7a92da
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-06-24 10:38:40 -0700

    wip

--- GANs/CycleGAN/inference.py
+++ GANs/CycleGAN/inference.py
@@ -12,7 +12,7 @@ def main():
     generator_a2b = make_generator_model(n_blocks=9)
     generator_b2a = make_generator_model(n_blocks=9)
 
-    ds = 'monet2photo'
+    ds = 'celeba'
     checkpoint_dir = './checkpoints-{}'.format(ds)
     checkpoint = tf.train.Checkpoint(generator_a2b=generator_a2b,
                                      generator_b2a=generator_b2a)
@@ -23,13 +23,13 @@ def main():
     else:
         print("Initializing from scratch.")
 
-    # Load TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path="photo2monet_converted_model.tflite")
-    interpreter.allocate_tensors()
-
-    # Get input and output tensors.
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+    # # Load TFLite model and allocate tensors.
+    # interpreter = tf.lite.Interpreter(model_path="photo2monet_converted_model.tflite")
+    # interpreter.allocate_tensors()
+    #
+    # # Get input and output tensors.
+    # input_details = interpreter.get_input_details()
+    # output_details = interpreter.get_output_details()
 
     def generate(filename, idx, kind):
         with open(filename, 'rb') as f:
@@ -41,10 +41,10 @@ def main():
         inputs = tf.expand_dims(inputs, 0)
 
         # outputs = generator_b2a(inputs)
-        # outputs = generator_a2b(inputs)
-        interpreter.set_tensor(input_details[0]['index'], inputs)
-        interpreter.invoke()
-        outputs = interpreter.get_tensor(output_details[0]['index'])
+        outputs = generator_a2b(inputs)
+        # interpreter.set_tensor(input_details[0]['index'], inputs)
+        # interpreter.invoke()
+        # outputs = interpreter.get_tensor(output_details[0]['index'])
 
         generated = outputs[0]
         generated = (generated + 1) * 127.5
@@ -55,21 +55,21 @@ def main():
         # plt.show()
         # print(original)
         im1 = Image.fromarray(original.numpy())
-        im1.save('samples_{}/custom_{}_{}_original.JPEG'.format(ds, kind, idx))
-        # generated = tf.image.resize(generated, (218, 178))
+        im1.save('samples_{}/male2female_{}_{}_original.JPEG'.format(ds, kind, idx))
+        generated = tf.image.resize(generated, (218, 178))
         generated = tf.cast(generated, tf.uint8)
         im2 = Image.fromarray(generated.numpy())
-        im2.save('samples_{}/custom_{}_{}_generated.JPEG'.format(ds, kind, idx))
+        im2.save('samples_{}/male2female_{}_{}_generated.JPEG'.format(ds, kind, idx))
 
-    files = glob.glob(os.path.join('datasets', ds, 'customB', '*'))
+    files = glob.glob(os.path.join('datasets', ds, 'trainA', '*'))
     # generate('/Users/yanjia.li/Downloads/img_align_celeba/000030.jpg', 30, 'male2female')
-    for idx in range(2):
-        generate(files[idx], idx, 'photo2monet')
+    for idx in range(20):
+        generate(files[idx], idx, 'celeba')
         # generate(files[idx], idx, 'monet2photo')
 
     # tf.saved_model.save(generator_a2b, "./saved_models/celeba/male2female/201905282111/")
 
-    tf.saved_model.save(generator_b2a, "./saved_models/monet2photo/photo2monet/201905291209/")
+    # tf.saved_model.save(generator_b2a, "./saved_models/monet2photo/photo2monet/201905291209/")
 
 
 if __name__ == '__main__':
