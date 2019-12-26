184745b9ebb6e2a6641895e1d12ad5b596371612
Ethan Yanjia Li <liyanjia92@gmail.com>
2019-12-26 11:58:23 -0800

    80 classes

--- YOLO/tensorflow/train.py
+++ YOLO/tensorflow/train.py
@@ -10,7 +10,7 @@ from yolov3 import YoloV3, YoloLoss, anchors_wh
 from preprocess import Preprocessor
 
 BATCH_SIZE = 16
-TOTAL_CLASSES = 1
+TOTAL_CLASSES = 80
 TOTAL_EPOCHS = 300
 OUTPUT_SHAPE = (416, 416)
 TF_RECORDS = './dataset/tfrecords'
@@ -30,12 +30,15 @@ class Trainer(object):
         self.strategy = strategy
         self.global_batch_size = global_batch_size
         self.loss_objects = [
-            YoloLoss(num_classes=TOTAL_CLASSES,
-                     valid_anchors_wh=anchors_wh[0:3]),  # small scale 52x52
-            YoloLoss(num_classes=TOTAL_CLASSES,
-                     valid_anchors_wh=anchors_wh[3:6]),  # medium scale 26x26
-            YoloLoss(num_classes=TOTAL_CLASSES,
-                     valid_anchors_wh=anchors_wh[6:9]),  # large scale 13x13
+            YoloLoss(
+                num_classes=TOTAL_CLASSES,
+                valid_anchors_wh=anchors_wh[0:3]),  # small scale 52x52
+            YoloLoss(
+                num_classes=TOTAL_CLASSES,
+                valid_anchors_wh=anchors_wh[3:6]),  # medium scale 26x26
+            YoloLoss(
+                num_classes=TOTAL_CLASSES,
+                valid_anchors_wh=anchors_wh[6:9]),  # large scale 13x13
         ]
         self.optimizer = tf.keras.optimizers.Adam(
             learning_rate=initial_learning_rate)
@@ -46,7 +49,7 @@ class Trainer(object):
         self.lowest_val_loss = math.inf
         self.patience_count = 0
         self.max_patience = 10
-    
+
     def lr_decay(self):
         """
         This effectively simulate ReduceOnPlateau learning rate schedule. Learning rate
@@ -60,8 +63,7 @@ class Trainer(object):
         self.patience_count += 1
 
         self.optimizer.learning_rate = self.current_learning_rate
-            
-        
+
     def train_step(self, inputs):
         images, labels = inputs
 
@@ -75,8 +77,7 @@ class Trainer(object):
             # iterate over all three scales
             for loss_object, y_pred, y_true in zip(self.loss_objects, outputs,
                                                    labels):
-                total_loss, loss_breakdown = loss_object(y_true,
-                                   y_pred)
+                total_loss, loss_breakdown = loss_object(y_true, y_pred)
                 xy_loss, wh_loss, class_loss, obj_loss = loss_breakdown
                 total_losses.append(total_loss * (1. / self.global_batch_size))
                 xy_losses.append(xy_loss * (1. / self.global_batch_size))
@@ -95,7 +96,8 @@ class Trainer(object):
         self.optimizer.apply_gradients(
             zip(grads, self.model.trainable_variables))
 
-        return total_loss, (total_xy_loss, total_wh_loss, total_class_loss, total_obj_loss)
+        return total_loss, (total_xy_loss, total_wh_loss, total_class_loss,
+                            total_obj_loss)
 
     def val_step(self, inputs):
         images, labels = inputs
@@ -105,12 +107,12 @@ class Trainer(object):
         # iterate over all three scales
         for loss_object, y_pred, y_true in zip(self.loss_objects, outputs,
                                                labels):
-            loss, _ = loss_object(y_true, y_pred) 
+            loss, _ = loss_object(y_true, y_pred)
             losses.append(loss * (1. / self.global_batch_size))
         total_loss = tf.reduce_sum(losses)
 
         return total_loss
-        
+
     def run(self, train_dist_dataset, val_dist_dataset):
         # @tf.function
         def distributed_train_epoch(dataset):
@@ -124,18 +126,31 @@ class Trainer(object):
                 batch_loss = self.strategy.reduce(
                     tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
                 batch_xy_loss = self.strategy.reduce(
-                    tf.distribute.ReduceOp.SUM, per_replica_xy_losses, axis=None)
+                    tf.distribute.ReduceOp.SUM,
+                    per_replica_xy_losses,
+                    axis=None)
                 batch_wh_loss = self.strategy.reduce(
-                    tf.distribute.ReduceOp.SUM, per_replica_wh_losses, axis=None)
+                    tf.distribute.ReduceOp.SUM,
+                    per_replica_wh_losses,
+                    axis=None)
                 batch_class_loss = self.strategy.reduce(
-                    tf.distribute.ReduceOp.SUM, per_replica_class_losses, axis=None)
+                    tf.distribute.ReduceOp.SUM,
+                    per_replica_class_losses,
+                    axis=None)
                 batch_obj_loss = self.strategy.reduce(
-                    tf.distribute.ReduceOp.SUM, per_replica_obj_losses, axis=None)
+                    tf.distribute.ReduceOp.SUM,
+                    per_replica_obj_losses,
+                    axis=None)
                 total_loss += batch_loss
                 num_train_batches += 1
                 t1 = time.time()
                 tf.print('Trained batch:', num_train_batches, 'batch loss:',
-                         batch_loss, 'batch xy loss', batch_xy_loss, 'batch wh loss', batch_wh_loss, 'batch obj loss', batch_obj_loss, 'batch_class_loss', batch_class_loss, 'epoch total loss:', total_loss, 'duration seconds:', round(t1-t0, 3), 'examples/seconds:', self.global_batch_size / (t1-t0))
+                         batch_loss, 'batch xy loss', batch_xy_loss,
+                         'batch wh loss', batch_wh_loss, 'batch obj loss',
+                         batch_obj_loss, 'batch_class_loss', batch_class_loss,
+                         'epoch total loss:', total_loss, 'duration seconds:',
+                         round(t1 - t0, 3), 'examples/seconds:',
+                         self.global_batch_size / (t1 - t0))
             return total_loss, num_train_batches
 
         @tf.function
@@ -150,43 +165,56 @@ class Trainer(object):
                 total_loss += batch_loss
                 num_val_batches += 1
             return total_loss, num_val_batches
-        
-        print('{} Start training...'.format(datetime.now().strftime("%m/%d/%Y %H:%M:%S")))
-        for epoch in range(1, self.epochs+1):
+
+        print('{} Start training...'.format(
+            datetime.now().strftime("%m/%d/%Y %H:%M:%S")))
+        for epoch in range(1, self.epochs + 1):
             t0 = time.time()
             self.lr_decay()
 
-            print('{}   Started epoch {} with learning rate {}. Current LR patience count is {} epochs. Last lowest val loss is {}.'.format(
-                datetime.now().strftime("%m/%d/%Y %H:%M:%S"), epoch, self.current_learning_rate, self.patience_count, self.lowest_val_loss))
+            print(
+                '{}   Started epoch {} with learning rate {}. Current LR patience count is {} epochs. Last lowest val loss is {}.'
+                .format(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), epoch,
+                        self.current_learning_rate, self.patience_count,
+                        self.lowest_val_loss))
 
             train_total_loss, num_train_batches = distributed_train_epoch(
                 train_dist_dataset)
             t1 = time.time()
             train_loss = train_total_loss / num_train_batches
-            print('{}   Epoch {} train loss {}, total train batches {}, {} examples per second'.format(
-                datetime.now().strftime("%m/%d/%Y %H:%M:%S"), epoch, train_loss, num_train_batches, num_train_batches*self.global_batch_size/(t1-t0)))
+            print(
+                '{}   Epoch {} train loss {}, total train batches {}, {} examples per second'
+                .format(
+                    datetime.now().strftime("%m/%d/%Y %H:%M:%S"), epoch,
+                    train_loss, num_train_batches,
+                    num_train_batches * self.global_batch_size / (t1 - t0)))
 
             val_total_loss, num_val_batches = distributed_val_epoch(
                 val_dist_dataset)
-            
+
             t2 = time.time()
             val_loss = val_total_loss / num_val_batches
-            print('{}   Epoch {} val loss {}, total val batches {}, {} examples per second'.format(
-                datetime.now().strftime("%m/%d/%Y %H:%M:%S"), epoch, val_loss, num_val_batches, num_val_batches*self.global_batch_size/(t2-t1)))
-            
+            print(
+                '{}   Epoch {} val loss {}, total val batches {}, {} examples per second'
+                .format(datetime.now().strftime("%m/%d/%Y %H:%M:%S"), epoch,
+                        val_loss, num_val_batches,
+                        num_val_batches * self.global_batch_size / (t2 - t1)))
+
             # save model when reach a new lowest validation loss
             if val_loss < self.lowest_val_loss:
                 self.save_model(epoch, val_loss)
                 self.lowest_val_loss = val_loss
             self.last_val_loss = val_loss
-            
 
         self.save_model(self.epochs, self.last_val_loss)
-        print('{} Finished.'.format(datetime.now().strftime("%m/%d/%Y %H:%M:%S")))
+        print('{} Finished.'.format(
+            datetime.now().strftime("%m/%d/%Y %H:%M:%S")))
 
     def save_model(self, epoch, loss):
         # https://github.com/tensorflow/tensorflow/issues/33565
-        self.model.save_weights('./models/model-v1.0.8-epoch-{}-loss-{:.4f}.tf'.format(epoch, loss))
+        self.model.save_weights(
+            './models/model-v1.0.8-epoch-{}-loss-{:.4f}.tf'.format(
+                epoch, loss))
         print("Model saved")
 
 
@@ -221,7 +249,8 @@ def main():
             train_dataset)
         val_dist_dataset = strategy.experimental_distribute_dataset(
             val_dataset)
-        model = YoloV3(shape=(416, 416, 3), num_classes=TOTAL_CLASSES, training=True)
+        model = YoloV3(
+            shape=(416, 416, 3), num_classes=TOTAL_CLASSES, training=True)
         model.summary()
 
         trainer = Trainer(
